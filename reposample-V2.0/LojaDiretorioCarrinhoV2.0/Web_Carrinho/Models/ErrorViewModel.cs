6d13a12c6b989abb87737fc81c29868528650f3c
Guilherme Franco de Moraes <110939274+PowerKombate@users.noreply.github.com>
2023-02-28 16:07:17 -0300

    Delete reposample-V2.0/LojaDiretorioCarrinhoV2.0 directory

--- reposample-V2.0/LojaDiretorioCarrinhoV2.0/Web_Carrinho/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Web_Carrinho.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
