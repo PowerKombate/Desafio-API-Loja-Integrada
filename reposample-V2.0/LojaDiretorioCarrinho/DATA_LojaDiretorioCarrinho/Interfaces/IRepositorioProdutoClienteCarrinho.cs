0744bb1c93bc551f4dd3519d7d117b08dc4911e1
Guilherme Franco de Moraes <110939274+PowerKombate@users.noreply.github.com>
2023-02-28 15:49:51 -0300

    2º Versão do Projeto
    Versão usando ASP.NET, Modelo MVC e Integração com Banco de Dados

---
+++ reposample-V2.0/LojaDiretorioCarrinho/DATA_LojaDiretorioCarrinho/Interfaces/IRepositorioProdutoClienteCarrinho.cs
@@ -0,0 +1,15 @@
+﻿using DATA_LojaDiretorioCarrinho.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATA_LojaDiretorioCarrinho.Interfaces
+{
+    // Interface do Relacionamento do Produto com o Cliente, utilizando a variável genérica (Referenciando o Modelo).
+    public interface IRepositorioProdutoClienteCarrinho : IModeloRepositorio<ProdutoClienteCarrinho>
+    {
+
+    }
+}
