d9074212c48c2fe2a84d2ded5f3e882fcde57bcc
Guilherme Franco de Moraes <110939274+PowerKombate@users.noreply.github.com>
2023-02-28 16:11:59 -0300

    2º Versão do Projeto V2.0
    Correção de Bugs e Hot Fix's

---
+++ reposample-V2.0/LojaDiretorioCarrinhoV2.0/LojaDiretorioCarrinho/Web_Carrinho/Controllers/CarrinhoController.cs
@@ -0,0 +1,52 @@
+﻿using Data_LojaDiretorioCarrinho.Models;
+using Data_LojaDiretorioCarrinho.Servicos;
+using Microsoft.AspNetCore.Mvc;
+using Web_Carrinho.Models;
+
+namespace Web_Carrinho.Controllers
+{
+    public class CarrinhoController : Controller
+    {
+        private ProdutoClienteCarrinhoService _service = new ProdutoClienteCarrinhoService();
+        public IActionResult Index()
+        {
+            List<VwProdutoClienteCarrinho> oListVwProdutoClienteCarrinho = _service.oRepositorioVwProdutoClienteCarrinho.SelecionarTodos();
+            return View(oListVwProdutoClienteCarrinho);
+        }
+
+        public IActionResult Create()
+        {
+            CarrinhoViewModel oCarrinhoViewModel = new CarrinhoViewModel();
+            List<Produto> oListProduto = _service.oRepositorioProduto.SelecionarTodos();
+            List<Cliente> oListCliente = _service.oRepositorioCliente.SelecionarTodos();
+
+            oCarrinhoViewModel.oListCliente = oListCliente;
+            oCarrinhoViewModel.oListProduto = oListProduto;
+
+            oCarrinhoViewModel.DataCompra = DateTime.Now;
+            oCarrinhoViewModel.Frete = 0;
+
+            return View(oCarrinhoViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Create(CarrinhoViewModel oCarrinhoViewModel)
+        {
+            ProdutoClienteCarrinho oProdutoClienteCarrinho = new ProdutoClienteCarrinho();
+            oProdutoClienteCarrinho.DataCompra = oCarrinhoViewModel.DataCompra;
+            oProdutoClienteCarrinho.Frete = oCarrinhoViewModel.Frete;
+            oProdutoClienteCarrinho.IdCliente = oCarrinhoViewModel.IdCliente;
+            oProdutoClienteCarrinho.IdProduto = oCarrinhoViewModel.IdProduto;
+            
+
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            _service.oRepositorioProdutoClienteCarrinho.Incluir(oProdutoClienteCarrinho);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
